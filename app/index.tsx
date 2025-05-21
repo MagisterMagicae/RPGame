1fd53b782aeb3c2d3acca86634f89d1edc980a03
unknown <tj.mm.email@gmail.com>
2025-05-22 01:39:22 +0200

    ADD page navigation

--- app/index.tsx
+++ app/index.tsx
@@ -1,65 +1,16 @@
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import * as React from 'react';
+import FightScreen from './fight_screen';
+import StartScreen from './start_screen';
 
-export default function Index() {
+
+const Pages = createNativeStackNavigator();
+
+export default function Index (){
   return (
-    <View style={styles.container}>
-      <View style={styles.titleContainer}>
-        <Text style={styles.title}>RPG</Text>
-        <Text style={styles.subtitle}>Game</Text>
-      </View>
-      
-      <View style={styles.buttonContainer}>
-        <TouchableOpacity style={styles.button}>
-          <Text style={styles.buttonText}>spielen</Text>
-        </TouchableOpacity>
-        
-        <TouchableOpacity style={styles.button}>
-          <Text style={styles.buttonText}>Spielstand löschen</Text>
-        </TouchableOpacity>
-        
-        <TouchableOpacity style={styles.button}>
-          <Text style={styles.buttonText}>Credits</Text>
-        </TouchableOpacity>
-      </View>
-    </View>
+       <Pages.Navigator>
+        <Pages.Screen name = "StartScreen" component={StartScreen} />
+        <Pages.Screen name = "FightScreen" component={FightScreen} />
+      </Pages.Navigator>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: "center",
-    alignItems: "center",
-    backgroundColor: "#fff",
-  },
-  titleContainer: {
-    alignItems: "center",
-    marginBottom: 40,
-  },
-  title: {
-    fontSize: 48,
-    fontWeight: "bold",
-    color: "#333",
-  },
-  subtitle: {
-    fontSize: 24,
-    color: "#666",
-    marginTop: 8,
-  },
-  buttonContainer: {
-    width: "80%",
-    gap: 16,
-  },
-  button: {
-    backgroundColor: "#4a90e2",
-    padding: 15,
-    borderRadius: 8,
-    width: "100%",
-  },
-  buttonText: {
-    color: "white",
-    fontSize: 18,
-    textAlign: "center",
-    fontWeight: "500",
-  },
-});
