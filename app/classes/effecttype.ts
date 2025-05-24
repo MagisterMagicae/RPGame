94ac8349cde013939b31a54b795e093cabb70eba
alxkten <alex.kechter@stud.hshl.de>
2025-05-25 00:21:18 +0200

    ADD item, effecttype classes

---
+++ app/classes/effecttype.ts
@@ -0,0 +1,8 @@
+export enum EffectType{
+    SCHWERT = 0, //Bei Angabe von einem Startwert wird der Rest jeweils um 1 autoinkrementiert
+    BOGEN,
+    STAB,
+    HEILUNG,
+    ATKBOOST,
+    DEFBOOST
+}
