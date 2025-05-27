a5207bd6301315a83cb68c4de0f2f1485f123633
unknown <tj.mm.email@gmail.com>
2025-05-27 02:08:42 +0200

    New Assets

---
+++ styles/image_style.js
@@ -0,0 +1,13 @@
+import { StyleSheet } from "react-native";
+
+export const ImageStyles = StyleSheet.create({
+    monsterImage:{
+        width: 200,
+        resizeMode: 'contain',
+        marginVertical: -50
+    },
+    icon:{
+        width: 75,
+        resizeMode: 'contain'
+    }
+})
